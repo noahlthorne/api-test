27e011c9c57207f25b7dcc92255b715624304501
Noah Thorne <noahlthorne@gmail.com>
2020-02-06 13:20:33 +0900

    created models and controllers for users and posts

---
+++ db/migrate/20200206041233_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.references :user, foreign_key: true
+      t.text :content
+      t.integer :likes
+      t.integer :retweets
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+30.times do
+  user = User.new
+  user.email = Faker::Internet.email
+  user.encrypted_password = '#$taawktljasktlw4aaglj'
+  user.save!
+end
+
+# User.all.each do |user|
+#   Post.create(
+
+#   )
+# end
