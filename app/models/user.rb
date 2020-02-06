38c9184ea5680d9f7f1d1c14c785ead2bf69cdd1
Noah Thorne <noahlthorne@gmail.com>
2020-02-06 17:13:54 +0900

    created cards for post index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def random_posts
+    posts.sample
+  end
+
   acts_as_favoritable
   has_many :posts, dependent: :destroy
   devise :database_authenticatable, :registerable,
