e5abe8a60d8bc9673e99ac1d4f47a5c44dc97021
Noah Thorne <noahlthorne@gmail.com>
2020-02-06 16:32:03 +0900

    added posts index with partial for each post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,35 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_post, only: [:show, :create]
+
   def index
     @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to @post, notice: 'Your post was successfully published'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  acts_as_favoritor
+  acts_as_favoritable
   has_many :posts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
