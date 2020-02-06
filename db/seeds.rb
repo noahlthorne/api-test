53650a4d0d146519f6bc1c01d4015b339aa55cdd
Noah Thorne <noahlthorne@gmail.com>
2020-02-06 15:30:19 +0900

    added seeds to database for users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,28 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+puts "destroying old users..."
 User.destroy_all
-30.times do
+puts "creating new fake users..."
+50.times do
   user = User.new
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
   user.email = Faker::Internet.email
-  user.encrypted_password = '#$taawktljasktlw4aaglj'
+  user.password = '#$taawktljasktlw4aaglj'
   user.save!
 end
 
-# User.all.each do |user|
-#   Post.create(
+User.all.each do |user|
+  rand(1..50).times do
+    Post.create!(
+      user: user,
+      content: Faker::Food.description,
+      likes: rand(1..999),
+      retweets: rand(1..999),
+    )
+  end
+  puts "created #{user.posts.count} posts for #{user.first_name} #{user.last_name}"
+end
 
-#   )
-# end
+puts "created #{User.count} users and #{Post.count} total posts!"
