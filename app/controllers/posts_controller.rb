a47fdfc0f47b515a3245d2ecf60bd3f9f0e9c54e
Noah Thorne <noahlthorne@gmail.com>
2020-02-08 18:12:42 +0900

    updated post index with buttons for removing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  before_action :set_post, only: [:show, :create]
+  before_action :set_post, only: [:show, :destroy]
 
   def index
     @posts = Post.all
@@ -18,10 +18,15 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user = current_user
     if @post.save
-      redirect_to @post, notice: 'Your post was successfully published'
+      redirect_to post_path(@post), notice: 'Your post was successfully published'
     else
       render :new
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: 'post deleted'
   end
 
   private
