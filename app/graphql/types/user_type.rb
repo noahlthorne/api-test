e6defda88c5a5de4fcf398c3777e9257134d47d8
Noah Thorne <noahlthorne@gmail.com>
2020-03-09 13:25:52 +0900

    added graphql fields for user

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,11 @@
+class Types::UserType < Types::BaseObject
+  description new_description "A user"
+
+  field :id, ID, null: false
+  field :first_name, String, null: true
+  field :last_name, String, null: true
+  field :full_name, String, null: true
+  field :photo, String, null: true
+  field :follower_count, Int, null: true
+  field :following_count, Int, null: true
+end
