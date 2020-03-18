560036c3ba006388fbc82a18f802daeca1da0f54
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-03-18 18:17:35 +0900

    removed graphql test field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,15 +4,6 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator" do
-      argument :name, String, required: true
-    end
-
-    def test_field(name:)
-      Rails.logger.info context.inspect
-      "Hello #{name}"
-    end
 
     field :user, Types::UserType, null: true, description: "A user" do
       argument :id, ID, required: true
